38a2cc7cea589ea1214ce1ace8911eeee1333330
vzakharchenko <vzakharchenko@modeln.com>
2021-07-07 22:13:25 +0300

    moved lambda@edge to https://github.com/vzakharchenko/keycloak-api-gateway

--- src/utils/TokenUtils.js
+++ src/utils/TokenUtils.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 const { lambdaAdapter } = require('../adapter/adapter');
 const { keycloakRefreshToken } = require('../clientAuthorization');
-const { tenantName } = require('../edge/lambdaEdgeUtils');
 
 function decodeAccessToken(externalToken) {
   const token = externalToken.access_token ? externalToken.access_token : externalToken;
@@ -21,41 +20,7 @@ async function tokenIsValid(token, options) {
   }
 }
 
-async function getActiveToken(session, accessToken, options, refreshTokenHandler) {
-  try {
-    await lambdaAdapter(decodeAccessToken(accessToken).accessToken,
-      options.keycloakJson,
-      options);
-    return accessToken;
-  } catch (e1) {
-    const sessionStorageItem = await options.sessionManager.getSessionIfExists(session, options);
-    if (sessionStorageItem) {
-      const tn = tenantName(options.keycloakJson(options));
-      const externalToken = sessionStorageItem[tn];
-      try {
-        const token = await keycloakRefreshToken(externalToken, options);
-        if (!token) {
-          return null;
-        }
-        if (refreshTokenHandler) {
-          refreshTokenHandler(token);
-        }
-        await options.sessionManager.updateSession(session,
-          tn,
-          token, options);
-        return token.access_token;
-      } catch (ex) {
-        throw new Error(ex);
-      }
-    } else {
-      options.logger.error(`Session does not exist: ${JSON.stringify(session)}`);
-      return null;
-    }
-  }
-}
-
 module.exports = {
   decodeAccessToken,
   tokenIsValid,
-  getActiveToken,
 };

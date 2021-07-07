5432099c1427f53de5d1ad1817d053d6fc77f1ee
vzakharchenko <vzakharchenko@modeln.com>
2021-07-07 22:21:00 +0300

    update dependencies

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const apigateway = require('./src/apigateway/apigateway');
 const adapter = require('./src/adapter/adapter');
 const { middlewareAdapter } = require('./src/adapter/middlewareAdapter');
-const lamdaEdge = require('./src/edge/lamdaEdge');
 
 module.exports = {
-  apigateway, adapter, lamdaEdge, middlewareAdapter,
+  apigateway, adapter, middlewareAdapter,
 };
